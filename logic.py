d3a849237601d2e0f5cf39f8fae2bae5fe2832e7
Applejeka <vtatarincew@gmail.com>
2024-12-20 04:50:39 +0700

    changed ui, added delete function, all works

--- logic.py
+++ logic.py
@@ -17,3 +17,41 @@ def add_data(connect_db, username, password):
     else:
         print("Пожалуйста, введите логин и пароль.")
 
+def delete_data(connect_db, username, password):
+    if username and password:
+        conn = connect_db()
+        if conn:
+            with conn.cursor() as cursor:
+                # Проверка правильности пароля
+                cursor.execute(
+                    sql.SQL('SELECT password FROM users WHERE username = %s'),
+                    (username,)
+                )
+                result = cursor.fetchone()
+                
+                if result and result[0] == password:
+                    # Удаление логина, если пароль правильный
+                    cursor.execute(
+                        sql.SQL('DELETE FROM users WHERE username = %s'),
+                        (username,)
+                    )
+                    conn.commit()
+                    if cursor.rowcount > 0:
+                        print("Логин удален:", username)
+                    else:
+                        print("Логин не найден:", username)
+                else:
+                    print("Неверный пароль логина:", username)
+            conn.close()
+    else:
+        print("Пожалуйста, логин и пароль.")
+
+def fetch_users(connect_db):
+    users = []
+    conn = connect_db()
+    if conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT username, password FROM users")
+            users = cursor.fetchall()
+        conn.close()
+    return users
--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 from logic import add_data
+from logic import delete_data
+from logic import fetch_users
 from database import connect_db
 
 def on_submit(entry_username, entry_password, connect_db):
@@ -8,58 +10,63 @@ def on_submit(entry_username, entry_password, connect_db):
     add_data(connect_db, username, password)
     entry_username.delete(0, ctk.END)
     entry_password.delete(0, ctk.END)
+    
+def on_delete(entry_username, entry_password, connect_db):
+    username = entry_username.get()
+    password = entry_password.get()
+    delete_data(connect_db, username, password)
+    entry_username.delete(0, ctk.END)
+    entry_password.delete(0, ctk.END)
 
 def create_main_window():
     root = ctk.CTk()
     root.title('Password manager')
-    root.geometry('900x600')
+    root.geometry('800x500')
     root.grid_columnconfigure(4, weight=1)
     root.grid_rowconfigure(1, weight=1)
     frame = ctk.CTkFrame(root)
-    frame.grid()
-    create_header(root)
+    frame.grid(row=1,column=0,padx=1, pady=1, sticky="ew")
+    create_header(frame)
     # create_left_side(root)
-    create_center(root)
+    user_list(frame)
     # create_right_side(root)
     root.mainloop()
 
-def create_header(root):#HEADER
-    header = ctk.CTkFrame(root)
-    header.grid_columnconfigure(4, weight=10)
+#Header/Add-Delete_login
+def create_header(frame):#HEADER
+    header = ctk.CTkFrame(frame)
+    header.grid_columnconfigure(5, weight=10)
     header.grid_rowconfigure(2, weight=10)
-
-    header_label = ctk.CTkLabel(header, text="Login")
-    header_label.grid(row=0,column=0,padx=10, pady=10)
-    header_label = ctk.CTkLabel(header, text="Password")
-    header_label.grid(row=0,column=2,padx=10, pady=10)
+    login_label = ctk.CTkLabel(header, text="Login")
+    login_label.grid(row=0,column=0,padx=10, pady=10)
+    password_label = ctk.CTkLabel(header, text="Password")
+    password_label.grid(row=0,column=2,padx=10, pady=10)
     #Поля логина и пороля
     entry_username = ctk.CTkEntry(header)
     entry_username.grid(row=0, column=1,padx=10, pady=10) #Login GRID
     entry_password = ctk.CTkEntry(header, show="*")
     entry_password.grid(row=0, column=3,padx=10, pady=10) #Password GRID
     #Кнопка добавления
-    button = ctk.CTkButton(root, text="Добавить", command=lambda: on_submit(entry_username, entry_password, connect_db))
-    button.grid(row=0, column=3,padx=10, pady=10)
-    #Кнопка удаления
-    button = ctk.CTkButton(root, text="Удалить", command=lambda: on_submit(entry_username, entry_password, connect_db))
+    button = ctk.CTkButton(header, text="Добавить", command=lambda: on_submit(entry_username, entry_password, connect_db))
     button.grid(row=0, column=4,padx=10, pady=10)
-
+    #Кнопка удаления
+    button = ctk.CTkButton(header, text="Удалить", command=lambda: on_delete(entry_username, entry_password, connect_db))
+    button.grid(row=0, column=5,padx=10, pady=10)
     header.grid(row=0, column=0,padx=10, pady=10, sticky="ew")
 
-# def create_left_side(root):#LEFT SIDE
-#     left_side = ctk.CTkFrame(root)
-#     left_label = ctk.CTkLabel(left_side, text="Left side")
-#     left_label.grid(row=0,column=0,padx=10, pady=10)
-#     left_side.grid(row=1, column=0,padx=10, pady=10, sticky="nsw")
+#Center/User_list
+def show_users(user_list):
+    users = fetch_users(connect_db)
+    user_list.delete(1.0, ctk.END)
+    for username, password in users:
+        user_list.insert(ctk.END, f"Username: {username}, Password: {password}\n")
+#CENTER
+def user_list(frame):
+    center = ctk.CTkFrame(frame)
+    button = ctk.CTkButton(center, text="Показать Пороли", command=lambda: show_users(user_list))
+    button.grid(row=1, column=1,padx=10, pady=10)
 
-def create_center(root):#CENTER
-    center = ctk.CTkFrame(root)
-    center_label = ctk.CTkLabel(center, text="Center side")
-    center_label.grid(row=0,column=0,padx=10, pady=10)
-    center.grid(row=1, column=1,padx=0, pady=10, sticky="nsew")
+    user_list = ctk.CTkTextbox(center, width=400, height=300)
+    user_list.grid(row=1, column=0,padx=10, pady=10)
 
-# def create_right_side(root):#RIGHT SIDE
-#     right_side = ctk.CTkFrame(root)
-#     right_label = ctk.CTkLabel(right_side, text="Right side")
-#     right_label.grid(row=0,column=0,padx=10, pady=10)
-#     right_side.grid(row=1, column=2,padx=10, pady=10, sticky="nse")
+    center.grid(row=1, column=0,padx=10, pady=10, sticky="nsw")
