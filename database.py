da5af31071c8fa68ccdbb00384093f059225196b
Applejeka <vtatarincew@gmail.com>
2024-12-20 18:40:02 +1000

    changed database to sqlite

--- database.py
+++ database.py
@@ -1,13 +1,22 @@
-import psycopg2
+import sqlite3
+
+# def connect_db():
+#     try:
+#         connect = sqlite3.connect(
+#             dbname='Password_manager',
+#             user='postgres',
+#             password='6813',
+#             host='localhost'
+#         )
+#         return connect
+#     except Exception as e:
+#         print(f"Ошибка подключения к базе данных: {e}")
+#         return None
+
 
 def connect_db():
     try:
-        connect = psycopg2.connect(
-            dbname='Password_manager',
-            user='postgres',
-            password='6813',
-            host='localhost'
-        )
+        connect = sqlite3.connect('/home/nuts/Documents/Database/Password_manager')
         return connect
     except Exception as e:
         print(f"Ошибка подключения к базе данных: {e}")
--- password_manager.py
+++ password_manager.py
@@ -1,40 +1,85 @@
+#from psycopg2 import sql
+import sqlite3
 from psycopg2 import sql
 from database import connect_db
+
+# def add_data(connect_db, username, password):
+#     if username and password:
+#         conn = connect_db()
+#         if conn:
+#             with conn.cursor() as cursor:
+#                 cursor.execute(
+#                     sql.SQL('INSERT INTO users (username, password) VALUES (%s, %s)'),
+#                     (username, password)
+#                 )
+#                 conn.commit()
+#                 print("Данные добавлены:", username)
+#             conn.close()
+#     else:
+#         print("Пожалуйста, введите логин и пароль.")
 
 def add_data(connect_db, username, password):
     if username and password:
         conn = connect_db()
         if conn:
-            with conn.cursor() as cursor:
+            with conn:
+                cursor = conn.cursor()
                 cursor.execute(
-                    sql.SQL('INSERT INTO users (username, password) VALUES (%s, %s)'),
+                    'INSERT INTO users (username, password) VALUES (?, ?)',
                     (username, password)
                 )
-                conn.commit()
                 print("Данные добавлены:", username)
             conn.close()
     else:
         print("Пожалуйста, введите логин и пароль.")
 
+# def delete_data(connect_db, username, password):
+#     if username and password:
+#         conn = connect_db()
+#         if conn:
+#             with conn.cursor() as cursor:
+#                 # Проверка правильности пароля
+#                 cursor.execute(
+#                     sql.SQL('SELECT password FROM users WHERE username = %s'),
+#                     (username,)
+#                 )
+#                 result = cursor.fetchone()
+                
+#                 if result and result[0] == password:
+
+#                     cursor.execute(
+#                         sql.SQL('DELETE FROM users WHERE username = %s'),
+#                         (username,)
+#                     )
+#                     conn.commit()
+#                     if cursor.rowcount != 0:
+#                         print("Логин удален:", username)
+#                     else:
+#                         print("Логин не найден:", username)
+#                 else:
+#                     print("Неверный пароль логина:", username)
+#             conn.close()
+#     else:
+#         print("Пожалуйста, логин и пароль.")
+
 def delete_data(connect_db, username, password):
     if username and password:
         conn = connect_db()
         if conn:
-            with conn.cursor() as cursor:
+            with conn:
+                cursor = conn.cursor()
                 # Проверка правильности пароля
                 cursor.execute(
-                    sql.SQL('SELECT password FROM users WHERE username = %s'),
+                    'SELECT password FROM users WHERE username = ?',
                     (username,)
                 )
                 result = cursor.fetchone()
                 
                 if result and result[0] == password:
-
                     cursor.execute(
-                        sql.SQL('DELETE FROM users WHERE username = %s'),
+                        'DELETE FROM users WHERE username = ?',
                         (username,)
                     )
-                    conn.commit()
                     if cursor.rowcount != 0:
                         print("Логин удален:", username)
                     else:
@@ -43,13 +88,24 @@ def delete_data(connect_db, username, password):
                     print("Неверный пароль логина:", username)
             conn.close()
     else:
-        print("Пожалуйста, логин и пароль.")
+        print("Пожалуйста, введите логин и пароль.")
+
+# def fetch_users(connect_db):
+#     users = []
+#     conn = connect_db()
+#     if conn:
+#         with conn.cursor() as cursor:
+#             cursor.execute("SELECT username, password FROM users")
+#             users = cursor.fetchall()
+#         conn.close()
+#     return users
 
 def fetch_users(connect_db):
     users = []
     conn = connect_db()
     if conn:
-        with conn.cursor() as cursor:
+        with conn:
+            cursor = conn.cursor()
             cursor.execute("SELECT username, password FROM users")
             users = cursor.fetchall()
         conn.close()
